aa288256fa56216a504f8c6282cf2dc98e68759c
Nicholas Le <nle031@west-mec.org>
2019-03-26 17:46:10 -0700

    Updated Files
    Fixed five errors and added the switch type and select letter functions to the code.

--- pc_cword.js
+++ pc_cword.js
@@ -54,42 +54,120 @@ var currentLetter;
 var wordLetters;
 var acrossClue;
 var downClue;
-var typeDirection;
+var typeDirection = "right";
 
 window.onload = init;
 
 // sets up the initial conditions of the puzzle.
 function init() {
-      allLetters = document.querySelector("table#crossword span");
+      allLetters = document.querySelectorAll("table#crossword span");
       currentLetter = allLetters[0];
       var acrossID = currentLetter.dataset.clueA;
       var downID = currentLetter.dataset.clueD;
-      acrossClue = document.getElementById("across");
-      downClue = document.getElementById("down");
+      acrossClue = document.getElementById(currentLetter.dataset.clueA);
+      downClue = document.getElementById(currentLetter.dataset.clueD);
+      formatPuzzle(currentLetter);
+      for (var i = 0; i < allLetters.length; i++) {
+            allLetters[i].style.cursor = "pointer";
+            allLetters[i].onmousedown = function (e) {
+                  formatPuzzle(e.target)
+            };
+      }
+      document.onkeydown = selectLetter;
+
+      var typeImage = document.getElementById("directionImg");
+      typeImage.style.cursor = "pointer";
+      typeImage.onclick = switchTypeDirection;
+
+      document.getElementById("showErrors").onclick = function () {
+            for (var i = 0; i < allLetters.length; i++) {
+                  if (allLetters[i].textContent !== allLetters[i].dataset.letter) {
+                        allLetters[i].style.color = "red";
+                        setTimeout(function () {
+                              for (var i = 0; i < allLetters.length; i++) {
+                                    allLetters[i].style.color = "";
+                              }
+                        }, 3000);
+                  }
+            }
+      }
+
+      document.getElementById("showSolution").onclick = function () {
+            for (var i = 0; i < allLetters.length; i++) {
+                  allLetters[i].textContent = allLetters[i].dataset.letter;
+            }
+      };
 
 }
 // function will format the colors of the crossword table cells and the clues
 function formatPuzzle(puzzleLetter) {
       currentLetter = puzzleLetter;
-      for (var i = 0; i <= allLetters.length; i++) {
+      for (var i = 0; i < allLetters.length; i++) {
             allLetters[i].style.backgroundColor = "";
-            acrossClue = currentLetter.dataset.clueA;
-            acrossClue.style.color = "blue";
-            wordLetters = document.querySelectorAll("currentLetter.dataset.clueA");
-            wordLetters.style.backgroundColor = "rgb(231, 231, 255)";
       }
 
-      for (var i = 0; i <= allLetters.length; i++) {
+      acrossClue.style.color = "";
+      downClue.style.color = "";
+
+      if (currentLetter.dataset.clueA !== undefined) {
+            acrossClue = document.getElementById(currentLetter.dataset.clueA);
+            acrossClue.style.color = "blue";
+            wordLetters = document.querySelectorAll("[data-clue-a = " + currentLetter.dataset.clueA + "]");
+
+            for (var i = 0; i < wordLetters.length; i++) {
+                  wordLetters[i].style.backgroundColor = "rgb(231, 231, 255)";
+            }
+
+      }
+
+      for (var i = 0; i < allLetters.length; i++) {
             allLetters[i].style.backgroundColor = "";
-            downClue = currentLetter.dataset.clueD;
+      }
+      if (currentLetter.dataset.clueD !== undefined) {
+            downClue = document.getElementById(currentLetter.dataset.clueD);
             downClue.style.color = "red";
-            wordLetters = document.querySelectorAll("currentLetter.dataset.clueD");
-            wordLetters.style.backgroundColor = "rgb(255, 231, 231)";
+            wordLetters = document.querySelectorAll("[data-clue-d = " + currentLetter.dataset.clueD + "]");
+            for (var i = 0; i < wordLetters.length; i++) {
+                  wordLetters[i].style.backgroundColor = "rgb(255, 231, 231)";
+            }
+
+      }
+
+      if (typeDirection === "right") {
+            currentLetter.style.backgroundColor = "rgb(191,191,255)";
+      } else {
+            currentLetter.style.backgroundColor = "rgb(255,191,191)";
       }
 }
 
+// this function is to allow users to select puzzle cells using the keyboard.
+function selectLetter(e) {
+      var rightLetter = currentLetter.dataset.right;
+      var leftLetter = currentLetter.dataset.left;
+      var upLetter = currentLetter.dataset.up;
+      var downLetter = currentLetter.dataset.down;
 
+      var userKey = e.keyCode;
 
+      if (userKey == 37) {
+            formatPuzzle(leftLetter);
+      } else if (userKey == 38) {
+            formatPuzzle(upLetter);
+      } else if (userKey == 39 || 9) {
+            formatPuzzle(rightLetter);
+      } else if (userKey == 40 || 13) {
+            formatPuzzle(downLetter);
+      } else if (userKey == 8 || 46) {
+            currentLetter = "";
+      } else if (userKey == 32) {
+            switchTypeDirection();
+      }
+
+}
+
+function switchTypeDirection() {
+
+}
 
 
 
